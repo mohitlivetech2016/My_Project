344541ec2170b18c2c28adbf733d35138117771b
Android <mohitlivetech2016@gmail.com>
2016-11-08 17:17:13 +0530

    8 nov 5:16

--- app/src/main/java/com/my_company/my_project/framework/activity/MainActivity.java
+++ app/src/main/java/com/my_company/my_project/framework/activity/MainActivity.java
@@ -2,6 +2,9 @@ package com.my_company.my_project.framework.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.my_company.my_project.R;
 
@@ -11,5 +14,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id=item.getItemId();
+
+        if(id==R.id.id_icon_contact){
+
+            return true;
+        }
+        if(id==R.id.id_icon_edit){
+
+            return true;
+        }
+        if(id==R.id.id_icon_mail){
+
+            return true;
+        }
+        if(id==R.id.id_icon_share){
+
+            return true;
+        }
+        if(id==R.id.id_icon_upload){
+
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
